efd40f54ca663aea04e20533c4bde3fcd68805ad
Jack Della <jacko1394@me.com>
2020-01-12 18:50:00 +1100

    initial

---
+++ src/Raspberry/Xxx.cs
@@ -0,0 +1,19 @@
+﻿// Xxx.cs
+// MAGIQ Mobile
+// Created by Jack Della on 12/01/2020
+// Copyright © 2020 MAGIQ Software Ltd. All rights reserved.
+
+using System;
+using Magiq.Mobile.Extensions;
+
+namespace Raspberry {
+
+    public class Xxx {
+
+        internal static void Main() => Console.WriteLine(Board.Current.ToJson());
+
+
+
+    }
+
+}
